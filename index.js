1089cd615337a4400b88dc0ba181fb0fd12702ff
Himanshu Sharma <himanshu.st6@gmail.com>
2025-05-29 03:09:51 +0530

    Added middleware for response headers

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const routerHandler = require('./routes/handler');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+});
 app.use('/', routerHandler);
 
 const PORT = 4000;
