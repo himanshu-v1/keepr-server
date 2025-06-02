a12b34f3a9b9ccb4d349c09da345c92fac3aa08b
Himanshu Sharma <himanshu.st6@gmail.com>
2025-06-03 02:14:17 +0530

    Added user authentication with session and user specific data

--- utilities/connection.js
+++ utilities/connection.js
@@ -5,15 +5,10 @@ function getConnection(collectionName) {
     const configFilePath = '../config.json';
     const configData = getConfigData(configFilePath);
     
-    console.log('collection: ', collectionName+'');    //
     const client = new MongoClient(configData.connectStr);
     const db = client.db(configData.dbName);
     let collection = '';
     collection = db.collection(collectionName);
-        // const r = db.createCollection(collectionName);
-        // collection = db.collection(collectionName);
-
-    console.log('finale: ', { client, collection });            //
     return {client, collection};
 };
 
